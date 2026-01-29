ab6d85bac031341fda94750b9289252ab55139ec
Thomaswalsh1 <130932404+thomaswalsh1@users.noreply.github.com.>
2026-01-29 14:50:27 -0500

    Exercise restructuring

--- src/exercises.h
+++ src/exercises.h
@@ -2,14 +2,15 @@
 #ifndef EXERCISES_H
 #define EXERCISES_H
 
-typedef struct {
+typedef struct Exercise {
     const char *id;
     const char *title;
     const char *description;
     const char *lab_dir;
-    int (*validate)(void);
+    int (*validate)(struct Exercise *ex);
     const char *hint;
     const char *target_file;
+    char sandbox_dir[512];
     int is_enabled;
     int is_completed;
 } Exercise;
